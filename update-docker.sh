504ed7908d903a16f8f4451b559f62383a680f66
Kyle Godbey <kylegodbey@gmail.com>
2024-09-30 17:27:10 -0400

    Update update-docker.sh

--- update-docker.sh
+++ update-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker-compose down
-docker pull ghcr.io/The-Level-Density-Group/NLD_database_website:master
+docker pull ghcr.io/the-level-density-group/nld_database_website:master
 docker image prune -f
 docker-compose up -d
